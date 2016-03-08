eac3c4530f497fad3f86ffc7117fed98939af072
woodsaj <anthony@monkey.id.au>
2016-03-08 19:53:00 +0800

    add snapd config and helper script to build plugins

---
+++ snap/scripts/build.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+SNAPDIR=$GOPATH/src/github.com/intelsdi-x/snap
+
+$SNAPDIR/scripts/build-plugin.sh $SNAPDIR/build/plugin github.com/raintank/raintank-apps/snap/plugin/snap-publisher-rt-hostedtsdb
+
+$SNAPDIR/scripts/build-plugin.sh $SNAPDIR/build/plugin github.com/raintank/raintank-apps/snap/plugin/snap-collector-rt-gitstats
