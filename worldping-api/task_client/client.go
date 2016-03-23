a29834e8613805b0cb732f54cfe459d669ad6d33
woodsaj <anthony@monkey.id.au>
2016-03-23 22:19:34 +0800

    log task-server address being used at startup

--- worldping-api/task_client/client.go
+++ worldping-api/task_client/client.go
@@ -1,12 +1,14 @@
 package task_client
 
 import (
+	"github.com/grafana/grafana/pkg/log"
 	"github.com/raintank/raintank-apps/task-server/client"
 )
 
 var Client *client.Client
 
 func Init(addr, apiKey string, insecure bool) (err error) {
+	log.Info("setting taskServer address to: %s", addr)
 	Client, err = client.New(addr, apiKey, insecure)
 	return err
 }
