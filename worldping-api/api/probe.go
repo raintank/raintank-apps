fb258e10491c5a8861433cd265e46b61a9137985
woodsaj <anthony@monkey.id.au>
2016-03-23 22:20:12 +0800

    implement probe CRUB methods

--- worldping-api/api/probe.go
+++ worldping-api/api/probe.go
@@ -2,6 +2,7 @@ package api
 
 import (
 	"github.com/grafana/grafana/pkg/log"
+	"github.com/raintank/raintank-apps/task-server/api/rbody"
 	sModel "github.com/raintank/raintank-apps/task-server/model"
 	"github.com/raintank/raintank-apps/worldping-api/model"
 	"github.com/raintank/raintank-apps/worldping-api/task_client"
@@ -11,6 +12,7 @@ func GetProbes(ctx *Context, query model.GetProbesQuery) {
 	query.Owner = ctx.Owner
 	pQuery := sModel.GetAgentsQuery{
 		Name:    query.Name,
+		Metric:  "/worldping/*/*/ping/*",
 		Enabled: query.Enabled,
 		Public:  query.Public,
 		Tag:     query.Tag,
@@ -22,7 +24,13 @@ func GetProbes(ctx *Context, query model.GetProbesQuery) {
 	agents, err := task_client.Client.GetAgents(&pQuery)
 	if err != nil {
 		log.Error(3, "api.GetProbes failed. %s", err)
-		ctx.JSON(500, err)
+		switch err.(type) {
+		case rbody.ApiError:
+			ctx.JSON(err.(rbody.ApiError).Code, err.(rbody.ApiError).Message)
+		default:
+			ctx.JSON(500, err)
+		}
+		return
 	}
 	ctx.JSON(200, agents)
 
@@ -30,22 +38,89 @@ func GetProbes(ctx *Context, query model.GetProbesQuery) {
 
 func AddProbe(ctx *Context, p model.ProbeDTO) {
 	p.Owner = ctx.Owner
-	ctx.JSON(200, "OK")
+	agent := &sModel.AgentDTO{
+		Owner:         ctx.Owner,
+		Name:          p.Name,
+		Tags:          p.Tags,
+		Public:        p.Public,
+		Enabled:       p.Enabled,
+		EnabledChange: p.EnabledChange,
+		Online:        p.Online,
+		OnlineChange:  p.OnlineChange,
+	}
+	err := task_client.Client.AddAgent(agent)
+	if err != nil {
+		log.Error(3, "api.AddProbe failed. %s", err)
+		switch err.(type) {
+		case rbody.ApiError:
+			ctx.JSON(err.(rbody.ApiError).Code, err.(rbody.ApiError).Message)
+		default:
+			ctx.JSON(500, err)
+		}
+		return
+	}
+	p.Id = agent.Id
+	ctx.JSON(200, p)
 }
 
 func UpdateProbe(ctx *Context, p model.ProbeDTO) {
 	p.Owner = ctx.Owner
-	ctx.JSON(200, "OK")
+	agent := &sModel.AgentDTO{
+		Id:            p.Id,
+		Owner:         ctx.Owner,
+		Name:          p.Name,
+		Tags:          p.Tags,
+		Public:        p.Public,
+		Enabled:       p.Enabled,
+		EnabledChange: p.EnabledChange,
+		Online:        p.Online,
+		OnlineChange:  p.OnlineChange,
+	}
+	err := task_client.Client.UpdateAgent(agent)
+	if err != nil {
+		log.Error(3, "api.UpdateProbe failed. %s", err)
+		switch err.(type) {
+		case rbody.ApiError:
+			ctx.JSON(err.(rbody.ApiError).Code, err.(rbody.ApiError).Message)
+		default:
+			ctx.JSON(500, err)
+		}
+		return
+	}
+
+	ctx.JSON(200, p)
 
 }
 
 func GetProbeById(ctx *Context) {
-	//id := ctx.ParamsInt64(":id")
-	ctx.JSON(200, "OK")
+	id := ctx.ParamsInt64(":id")
+	agent, err := task_client.Client.GetAgentById(id)
+	if err != nil {
+		log.Error(3, "api.GetProbeById failed. %s", err)
+		switch err.(type) {
+		case rbody.ApiError:
+			ctx.JSON(err.(rbody.ApiError).Code, err.(rbody.ApiError).Message)
+		default:
+			ctx.JSON(500, err)
+		}
+		return
+	}
+
+	ctx.JSON(200, agent)
 }
 
 func DeleteProbe(ctx *Context) {
-	//id := ctx.ParamsInt64(":id")
+	id := ctx.ParamsInt64(":id")
+	err := task_client.Client.DeleteAgent(&sModel.AgentDTO{Id: id})
+	if err != nil {
+		log.Error(3, "api.DeleteProbe failed. %s", err)
+		switch err.(type) {
+		case rbody.ApiError:
+			ctx.JSON(err.(rbody.ApiError).Code, err.(rbody.ApiError).Message)
+		default:
+			ctx.JSON(500, err)
+		}
+		return
+	}
 	ctx.JSON(200, "OK")
-
 }
