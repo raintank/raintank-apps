c15edc52d948ad46e4b24fb47ec071ad16d508a7
woodsaj <anthony@monkey.id.au>
2016-04-20 22:38:43 +0800

    refactor build scripts to make it easier to build individual components

---
+++ scripts/build.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -x
+
+PKG=${1:-"task-server task-agent tsdb"}
+
+BASE=$(dirname $0)
+
+CODE_DIR=$(readlink -e "$BASE/../")
+
+CURRENT_PWD=$(pwd)
+cd $CODE_DIR
+
+GIT_HASH=$(git rev-parse HEAD)
+
+mkdir -p ${CODE_DIR}/build/bin/
+
+for VAR in $PKG; do
+	cd $CODE_DIR/$VAR
+	go build -ldflags "-X main.GitHash=$GIT_HASH" -o ${CODE_DIR}/build/bin/$VAR
+done
+
+cd $CURRENT_PWD
---
+++ scripts/build_plugins.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+BASE=$(dirname $0)
+$BASE/../snap/scripts/build.sh
