da47b7d420af7316210129e04657654dd3c6b506
woodsaj <anthony@monkey.id.au>
2016-04-22 22:14:38 +0800

    gofmt

--- worldping-api/model/probe.go
+++ worldping-api/model/probe.go
@@ -18,12 +18,12 @@ type ProbeDTO struct {
 }
 
 type GetProbesQuery struct {
-	Name    string `form:"name" url:"name,omitempty"`
-	Enabled string `form:"enabled" url:"enabled,omitempty"`
-	Public  string `form:"public" url:"public,omitempty"`
+	Name    string   `form:"name" url:"name,omitempty"`
+	Enabled string   `form:"enabled" url:"enabled,omitempty"`
+	Public  string   `form:"public" url:"public,omitempty"`
 	Tag     []string `form:"tag" url:"tag,omitempty"`
-	OrderBy string `form:"orderBy" url:"orderBy,omitempty"`
-	Limit   int    `form:"limit" url:"limit,omitempty"`
-	Page    int    `form:"page" url:"page,omitempty"`
-	OrgId   int64  `form:"-" url:"-"`
+	OrderBy string   `form:"orderBy" url:"orderBy,omitempty"`
+	Limit   int      `form:"limit" url:"limit,omitempty"`
+	Page    int      `form:"page" url:"page,omitempty"`
+	OrgId   int64    `form:"-" url:"-"`
 }
