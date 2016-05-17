e813f19b8f0c1b67eb93215a62d200be864cce3c
Anthony Woods <awoods@raintank.io>
2016-05-17 20:15:38 +0800

    update how we use tags and versioning.

--- scripts/package.sh
+++ scripts/package.sh
@@ -5,14 +5,12 @@ CODE_DIR=$(readlink -e "$BASE/../")
 
 BUILD=$CODE_DIR/build
 
-VERSION="0.0.3" # need an automatic way to do this again :-/
 ARCH="$(uname -m)"
-ITERATION=`date +%s`ubuntu1
-TAG="pkg-${VERSION}-${ITERATION}"
+VERSION=$(git describe --long)
 
 for VAR in task-server task-agent tsdb; do
 	NSQ_BUILD="${BUILD}/$VAR-${VERSION}"
-	NSQ_PACKAGE_NAME="${BUILD}/${VAR}-${VERSION}_${ITERATION}_${ARCH}.deb"
+	NSQ_PACKAGE_NAME="${BUILD}/${VAR}-${VERSION}_${ARCH}.deb"
 	mkdir -p ${NSQ_BUILD}/usr/bin
 	mkdir -p ${NSQ_BUILD}/etc/init
 	mkdir -p ${NSQ_BUILD}/etc/raintank
@@ -26,9 +24,7 @@ for VAR in task-server task-agent tsdb; do
 	cp ${BASE}/etc/${VAR}.ini ${NSQ_BUILD}/etc/raintank/
 	cp cp ${BUILD}/bin/$VAR ${NSQ_BUILD}/usr/bin
 	fpm -s dir -t deb \
-	  -v ${VERSION} -n ${VAR} -a ${ARCH} --iteration $ITERATION --description "Raintank $VAR" \
+	  -v ${VERSION} -n ${VAR} -a ${ARCH} --description "Raintank $VAR" \
 	  --deb-upstart ${BASE}/etc/init/${VAR} \
 	  -C ${NSQ_BUILD} -p ${NSQ_PACKAGE_NAME} .
 done
-
-git tag $TAG
