03b38447d0344d8d78f9655f30c59956e103df70
woodsaj <anthony@monkey.id.au>
2016-07-04 18:20:49 +0800

    update snap plugin build script

--- snap/scripts/build.sh
+++ snap/scripts/build.sh
@@ -5,13 +5,9 @@ CODE_DIR=${1:-$(readlink -e "$BASE/../../")}
 SNAPDIR=$GOPATH/src/github.com/intelsdi-x/snap
 BINDIR=${CODE_DIR}/build/plugins
 mkdir -p $BINDIR
-$SNAPDIR/scripts/build-plugin.sh $BINDIR github.com/raintank/raintank-apps/snap/plugin/snap-publisher-rt-hostedtsdb
 
-$SNAPDIR/scripts/build-plugin.sh $BINDIR github.com/raintank/raintank-apps/snap/plugin/snap-collector-rt-gitstats
-
-$SNAPDIR/scripts/build-plugin.sh $BINDIR github.com/raintank/raintank-apps/snap/plugin/snap-collector-worldping-ping
-
-$SNAPDIR/scripts/build-plugin.sh $BINDIR github.com/raintank/raintank-apps/snap/plugin/snap-collector-ns1
-
-$SNAPDIR/scripts/build-plugin.sh $BINDIR github.com/raintank/raintank-apps/snap/plugin/snap-collector-voxter
-
+cd $CODE_DIR/snap/plugin
+for p in *; do
+	cd $CODE_DIR/snap/plugin/$p
+	go build -a -ldflags "-w" -o $BINDIR/$p
+done
